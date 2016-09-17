885766c1e9528b7e82849ad87c81d981c089cacb
Joe Rickerby <joerick@mac.com>
2016-09-17 15:45:18 +0100

    Make drawing app nicer

--- drawing.tingapp/main.py
+++ drawing.tingapp/main.py
@@ -1,11 +1,41 @@
 import tingbot
 from tingbot import *
+from itertools import cycle
 
-screen.fill(color='blue')
+state = {
+    'previous_xy': None,
+    'color': 'black'
+}
+colors = cycle(['black', 'blue', 'green', 'orange'])
+
+screen.fill(color='white')
+
+@left_button.hold
+def clear():
+    screen.fill(color='white')
+
+@right_button.press
+def next_color():
+    state['color'] = colors.next()
+    screen.rectangle(
+        size=(25,15),
+        color=state['color'],
+        align='topright')
+        
+next_color()
 
 @touch()
-def on_touch(xy):
-    screen.rectangle(xy=xy, size=(5,5), color='white')
+def on_touch(xy, action):
+    if state['previous_xy']:
+        screen.line(
+            state['previous_xy'],
+            xy,
+            width=5,
+            color=state['color'])
 
-# run the app
+    if action == 'up':
+        state['previous_xy'] = None
+    else:
+        state['previous_xy'] = xy
+
 tingbot.run()
